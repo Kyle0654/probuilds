defbca628e5dca1ac39393f3251f7c72a53efc44
Nathan Bowhay <skienemy5555@gmail.com>
2015-08-27 12:10:36 -0700

    Finished ItemSetWriter
    Added Enum Extension class to allow you to get attribute values of enums
    (used when generating name for item set files).
    Added a ItemSetTester class to put all my testing code in so I don't
    have to change program.cs.
    ItemSetWriter now has function to read in an item set and write out an
    item set sharing a common naming convention (PB<ChampionKey><Map><Given
    Name>.json)

---
+++ ProBuilds/EnumExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBuilds
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Gets an attribute on an enum field value
+        /// </summary>
+        /// <typeparam name="T">The type of the attribute you want to retrieve</typeparam>
+        /// <param name="enumVal">The enum value</param>
+        /// <returns>The attribute of type T that exists on the enum value</returns>
+        /// <example>string desc = myEnumVariable.GetAttributeOfType<DescriptionAttribute>().Description;</example>
+        public static T GetAttribute<T>(this Enum enumVal) where T : Attribute
+        {
+            var type = enumVal.GetType();
+            var memberInfo = type.GetMember(enumVal.ToString());
+            var attributes = memberInfo[0].GetCustomAttributes(typeof(T), false);
+
+            // check if no attributes have been specified.
+            if (((Array)attributes).Length > 0)
+            {
+                return (T)attributes[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- ProBuilds/ItemSetWriter.cs
+++ ProBuilds/ItemSetWriter.cs
@@ -2,14 +2,15 @@
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json.Converters;
 using System.Runtime.Serialization;
+using System.IO;
 
 namespace ProBuilds
 {
+    /// <summary>
+    /// Structure that holds everything in an item set to write out to JSON
+    /// </summary>
     [JsonObject(MemberSerialization.OptIn)]
     public struct ItemSet
     {
@@ -113,6 +114,9 @@ namespace ProBuilds
         [JsonProperty("title")]
         public string title;
 
+        [JsonProperty("description")]
+        public string description;
+
         [JsonProperty("type")]
         [JsonConverter(typeof(StringEnumConverter))]
         public Type type;
@@ -137,6 +141,7 @@ namespace ProBuilds
         public ItemSet(string title = "")
         {
             this.title = title;
+            this.description = "";
             this.type = Type.Custom;
             this.map = Map.any;
             this.mode = Mode.any;
@@ -146,27 +151,96 @@ namespace ProBuilds
         }
     }
 
-    class ItemSetWriter
+    static class ItemSetTester
+    {
+        private static ItemSet createTestSet(string championKey = "Ashe")
+        {
+            ItemSet set;
+            set.title = championKey + " ProBuilds";
+            set.description = "this is a test.";
+            set.type = ItemSet.Type.Custom;
+            set.map = ItemSet.Map.SummonersRift;
+            set.mode = ItemSet.Mode.any;
+            set.priority = false;
+            set.sortrank = 0;
+            set.blocks = new List<ItemSet.Block>();
+            ItemSet.Block block;
+            block.type = "Starting Out";
+            block.recMath = true;
+            block.minSummonerLevel = -1;
+            block.maxSummonerlevel = -1;
+            block.showIfSummonerSpell = "";
+            block.hideIfSummonerSpell = "";
+            block.items = new List<ItemSet.Item>();
+            ItemSet.Item item;
+            item.id = "1001";
+            item.count = 2;
+            block.items.Add(item);
+            set.blocks.Add(block);
+
+            return set;
+        }
+
+        public static void testWrite()
+        {
+            string championKey = "Ashe";
+            ItemSet set = createTestSet(championKey);
+            bool success = ItemSetWriter.writeOutItemSet(set, championKey, "Boots");
+
+            Console.WriteLine(success ? "Wrote out set" : "Couldn't write set (Already exists?)");
+        }
+
+        public static void testReader()
+        {
+            string championKey = "Ashe";
+            ItemSet.Map map = ItemSet.Map.SummonersRift;
+            Nullable<ItemSet> set = ItemSetWriter.readInItemSet(championKey, map, "Boots");
+
+            if(set != null)
+            {
+                string JSON = JsonConvert.SerializeObject(set.Value, Formatting.Indented);
+                Console.Write(JSON);
+            }
+            else
+            {
+                Console.WriteLine("No item set found");
+            }
+        }
+    }
+
+    static class ItemSetWriter
     {
         /// <summary>
         /// Sub path to global item sets
         /// </summary>
         private const string GlobalSubPath = "Config\\Global\\Recommended\\";
+
         /// <summary>
         /// Sub path to champion item sets (use string.Format to replace {0} with champion key)
         /// </summary>
         private const string ChampionSubPath = "Config\\Champions\\{0}\\Recommended\\";
+
         /// <summary>
         /// Full file path to league of legends install directory
         /// </summary>
         private static string LeagueOfLegendsPath = "C:\\Riot Games\\League of Legends\\";
 
         /// <summary>
+        /// Prepend this to all file names written
+        /// </summary>
+        private const string FileNamePrefix = "PB";
+
+        /// <summary>
+        /// File extention to use for the file
+        /// </summary>
+        private const string FileExtention = ".json";
+
+        /// <summary>
         /// Gets the full directory path to where the item sets should go or are stored
         /// </summary>
         /// <param name="championKey">Key value for the champion you want the item set directory for or empty if global</param>
-        /// <returns>Full path to confi</returns>
-        private string getItemSetDirectory(string championKey = "")
+        /// <returns>Full path to config</returns>
+        private static string getItemSetDirectory(string championKey = "")
         {
             if (championKey == "")
                 return LeagueOfLegendsPath + GlobalSubPath;
@@ -205,6 +279,104 @@ namespace ProBuilds
                     object programLocation = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" + item).GetValue("InstallLocation");
                     LeagueOfLegendsPath = programLocation.ToString();
                     break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Make sure a directory exists and if not create it
+        /// </summary>
+        /// <param name="path">directory path to ensure</param>
+        private static void ensureDirectory(string path)
+        {
+            string dirPath = Path.GetDirectoryName(path);
+            if (!Directory.Exists(dirPath))
+                Directory.CreateDirectory(dirPath);
+        }
+
+        /// <summary>
+        /// Crate the item set file name based on our naming convention
+        /// </summary>
+        /// <param name="championKey">Key value for the champion you want the item set directory for or empty if global</param>
+        /// <param name="map">Map type this item is set for</param>
+        /// <param name="name">Custom name added to file name</param>
+        /// <returns>Name for file including file extention</returns>
+        public static string getItemSetFileName(string championKey, ItemSet.Map map, string name)
+        {
+            //Convert the enum value to the EnumMember string
+            var atr = map.GetAttribute<EnumMemberAttribute>();
+            string mapAbreviation = atr.Value.ToString();
+
+            return FileNamePrefix + championKey + mapAbreviation + name + FileExtention;
+        }
+
+        /// <summary>
+        /// Write out the item set to a json file in the league of legends item set directory for the given champion key
+        /// If directory doesn't exist it will be created so be careful
+        /// </summary>
+        /// <param name="itemSet">Item set to write out to a json file</param>
+        /// <param name="championKey">Key value for the champion you want the item set directory for or empty if global</param>
+        /// <returns>True if item set doesn't exists and was written, false if item set exists already</returns>
+        public static bool writeOutItemSet(ItemSet itemSet, string championKey = "", string name = "")
+        {
+            //Convert our item set to json
+            string JSON = JsonConvert.SerializeObject(itemSet, Formatting.Indented);
+
+            //Get the directory we are going to write out to
+            string itemSetDir = getItemSetDirectory(championKey);
+
+            //Make sure the directory exists
+            ensureDirectory(itemSetDir);
+
+            //Create our unique file name for the system
+            string fileName = itemSetDir + getItemSetFileName(championKey, itemSet.map, name);
+
+            //Check if we already have this file
+            if (File.Exists(fileName))
+                return false;
+
+            //Write out the file
+            using (FileStream file = File.Create(fileName))
+            {
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+                    writer.Write(JSON);
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Read in an item set from disk
+        /// </summary>
+        /// <param name="championKey">Key value for the champion you want the item set directory for or empty if global</param>
+        /// <param name="map">Map this item set is for (used in naming convention)</param>
+        /// <param name="name">Name for this item set file (used in naming convention)</param>
+        /// <returns>Item set if file was found or null if not</returns>
+        public static Nullable<ItemSet> readInItemSet(string championKey = "", ItemSet.Map map = ItemSet.Map.SummonersRift, string name = "")
+        {
+            //Get the directory we are going to write out to
+            string itemSetDir = getItemSetDirectory(championKey);
+
+            //Make sure the directory exists
+            ensureDirectory(itemSetDir);
+
+            //Create our unique file name for the system
+            string fileName = itemSetDir + getItemSetFileName(championKey, map, name);
+
+            //Make sure we have this file
+            if (!File.Exists(fileName))
+                return null;
+
+            //Read in the file
+            using (FileStream file = File.OpenRead(fileName))
+            {
+                using (StreamReader reader = new StreamReader(file))
+                {
+                    string JSON = reader.ReadToEnd();
+                    ItemSet itemSet = JsonConvert.DeserializeObject<ItemSet>(JSON);
+                    return itemSet;
                 }
             }
         }
